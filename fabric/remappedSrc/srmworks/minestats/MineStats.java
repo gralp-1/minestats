a95897c7f214c2f5316004fd4467989664e7c260
gralp <62028969+gralp-1@users.noreply.github.com>
2022-03-06 22:27:12 +0000

    Add TODO(#4)

--- fabric/remappedSrc/srmworks/minestats/MineStats.java
+++ fabric/remappedSrc/srmworks/minestats/MineStats.java
@@ -31,7 +31,7 @@ import org.apache.logging.log4j.Logger;
 
 
 // TODO(#3): Make this configurable.
-// TODO: Make it configurable on a per-player basis.
+// TODO(#4): Make it configurable on a per-player basis.
 // TODO: Keep code clean ❌
 // Fix armour array. ✅
 // Round coordinates. ✅
